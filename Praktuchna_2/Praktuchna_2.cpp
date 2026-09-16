dbd5a74f603bd24cacbdb5281c8981f77d0c5f92
FL0PP1K <ostap.kovalskij@kpk-lp.com.ua>
2026-09-17 00:08:14 +0300

    Створено абстрактний інтерфейсний клас суб'єкта та клас конкретного суб'єкта

--- Praktuchna_2/Praktuchna_2.cpp
+++ Praktuchna_2/Praktuchna_2.cpp
@@ -15,3 +15,58 @@ public:
         std::cout << "Клієнт [" << name << "] отримав повідомлення: " << message << "\n";
     }
 };
+class ISubject {
+public:
+    virtual ~ISubject() {}
+    virtual void attach(IObserver* observer) = 0;
+    virtual void detach(IObserver* observer) = 0;
+    virtual void notify() = 0;
+};
+class Store : public ISubject {
+private:
+    static const int MAX_CUSTOMERS = 10;
+    IObserver* observers[MAX_CUSTOMERS];
+    int current_count;
+    std::string current_promotion;
+public:
+    Store() : current_count(0) {
+        for (int i = 0; i < MAX_CUSTOMERS; ++i) {
+            observers[i] = nullptr;
+        }
+    }
+    void attach(IObserver* observer) override {
+        if (current_count < MAX_CUSTOMERS) {
+            observers[current_count] = observer;
+            current_count++;
+            std::cout << "Новий підписник доданий до бази.\n";
+        }
+        else {
+            std::cout << "База підписників переповнена.\n";
+        }
+    }
+    void detach(IObserver* observer) override {
+        for (int i = 0; i < current_count; ++i) {
+            if (observers[i] == observer) {
+                for (int j = i; j < current_count - 1; ++j) {
+                    observers[j] = observers[j + 1];
+                }
+                observers[current_count - 1] = nullptr;
+                current_count--;
+                std::cout << "Клієнт відписався від розсилки.\n";
+                return;
+            }
+        }
+    }
+    void notify() override {
+        for (int i = 0; i < current_count; ++i) {
+            if (observers[i] != nullptr) {
+                observers[i]->update(current_promotion);
+            }
+        }
+    }
+    void createPromotion(const std::string& promotion) {
+        current_promotion = promotion;
+        std::cout << "\n[Store] Увага! Зміна стану: оголошено нову акцію!\n";
+        notify();
+    }
+};
